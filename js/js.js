10a01b09735255f26eac0ce33ab7bfd03ecf3ee2
Reese Williams <rfwilliams11@gmail.com>
2020-02-08 13:31:21 -0800

    Animation updates

---
+++ js/js.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+/*! Fades in page on load */
+$('#jsFadein').css('display', 'none');
+$('#jsFadein').fadeIn(750);
+
+$('#jsFadein2').css('display', 'none');
+$('#jsFadein2').fadeIn(750);
+
+$('#slider').css('display', 'none');
+$('#slider').slideDown(500);
+
+});

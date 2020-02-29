1d6b6ca461f0da78ba7c5feebfb349c1df24d408
Reese Williams <rfwilliams11@gmail.com>
2020-02-29 15:06:19 -0800

    Updated Portfolio page

--- js/js.js
+++ js/js.js
@@ -10,4 +10,27 @@ $('#jsFadein2').fadeIn(750);
 $('#slider').css('display', 'none');
 $('#slider').slideDown(500);
 
+$(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('500');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('1500');
+            $('.filter').filter('.'+value).show('1500');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
 });
